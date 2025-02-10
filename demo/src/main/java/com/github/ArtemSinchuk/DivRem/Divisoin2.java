9d72e70d3d40e23b65a36e78038318ae04eb4632
Artem Sinchuk <158162880+ArtemSinchuk@users.noreply.github.com>
2025-02-10 15:36:33 +0100

    added Class for test divisiion

---
+++ demo/src/main/java/com/github/ArtemSinchuk/DivRem/Divisoin2.java
@@ -0,0 +1,16 @@
+package com.github.ArtemSinchuk.DivRem;
+
+import java.util.Scanner;
+
+public class Disivion2 {
+  public static void main(String[] args) {
+  Scanner s = new Scanner(System.in);
+  System.out.println("Enter two numbers:");
+  int num1 = s.nextInt();
+  int num2 = s.nextInt();
+  int quotient = num1 / num2;
+  int rem = num1 % num2;
+  System.out.println(quotient + "; R = " + rem);
+
+  }
+}
