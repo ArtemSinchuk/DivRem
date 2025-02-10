3d3fb59877f2fd17dd4abe1d1d5bb9bface998c3
Artem Sinchuk <158162880+ArtemSinchuk@users.noreply.github.com>
2025-02-10 15:35:22 +0100

    finished with layouts

--- demo/src/main/java/com/github/ArtemSinchuk/DivRem/GUI.java
+++ demo/src/main/java/com/github/ArtemSinchuk/DivRem/GUI.java
@@ -8,18 +8,20 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.JPanel;  
 import javax.swing.JTextField;
 
 public class GUI extends JFrame {
-  
+
   private JPanel mainPanel = new JPanel();
   private JLabel dividendLabel = new JLabel("Dividend");
   private JLabel divisorLabel = new JLabel("Divisor");
-  private JLabel quotientLabel = new JLabel("Result");
+  private JLabel quotientLabel = new JLabel("Result: ");
   private JTextField dividendField = new JTextField(10);
   private JTextField divisorField = new JTextField(10);
   private JButton calculateButton = new JButton("Calculate");
+  private GridLayout layout = new GridLayout(0, 1, 0, 0);
+  private ImageIcon logo = new ImageIcon("logo.png");
 
   public GUI() {
     runGUI();
@@ -27,6 +29,7 @@ public class GUI extends JFrame {
 
   private void runGUI() {
     frameConf();
+    panelConf();
     labelConf();
     textFieldConf();
     buttonConf();
@@ -35,28 +38,34 @@ public class GUI extends JFrame {
 
   private void frameConf() {
     setTitle("DivRem");
+    setIconImage(logo.getImage());
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setSize(300, 100);
-    setLayout(new GridLayout());
+    setSize(300, 250);
+    setLayout(new BorderLayout());
     setLocationRelativeTo(null);
     setVisible(true);
   }
 
   public void panelConf() {
-    mainPanel.setLayout(new GridLayout(3, 2));
+    mainPanel.setLayout(layout);
     mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
   }
 
-  private void labelConf() {
-
-  }
+  private void labelConf() {}
 
   private void textFieldConf() {
-    dividendField.setSize(75, 30);
+    dividendField.setSize(50, 10);
+    
   }
 
   private void buttonConf() {
-
+    calculateButton.addActionListener(ActionListener -> {
+      double dividend = Double.parseDouble(dividendField.getText());
+      double divisor = Double.parseDouble(divisorField.getText());
+      int quotient = (int) (dividend / divisor);
+      int remainder = (int) (dividend % divisor);
+      quotientLabel.setText("Result: " + quotient + "; R: " + remainder);
+    });
   }
 
   private void addComponents() {
