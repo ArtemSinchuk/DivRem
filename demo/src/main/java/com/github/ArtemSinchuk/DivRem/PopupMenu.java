1dd001555a71bd1ff0d95d0cc7700b88bdd12aee
Artem Sinchuk <158162880+ArtemSinchuk@users.noreply.github.com>
2025-02-11 15:35:27 +0100

    Create PopupMenu.java

---
+++ demo/src/main/java/com/github/ArtemSinchuk/DivRem/PopupMenu.java
@@ -0,0 +1,12 @@
+package com.github.artemsinchuk.divRem;
+
+import javax.swing.JPopupMenu;
+
+public class PopupMenu {
+  
+
+  public JPopupMenu getPopupMenu() {
+    JPopupMenu popupMenu = new JPopupMenu(); 
+    return popupMenu;
+  }
+}
